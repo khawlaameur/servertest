5856a51dac206613eddbbe77acab343cf0e36209
khawlaameur <95720186+khawlaameur@users.noreply.github.com>
2022-04-09 12:51:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require ("express")
+var app = express()
+app.get("/",function(req,res){
+   res.send("le server est en marche :) ")
+})
+app.listen(3000,()=>{
+console.log("server started")
+})
