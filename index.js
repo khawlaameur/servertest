fd26fe73610a2fb886375b84f07f12b1d25618bb
khawlaameur <95720186+khawlaameur@users.noreply.github.com>
2022-04-23 11:58:39 +0200

    mise à jours

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express()
 app.get("/",function(req,res){
    res.send("le server est en marche :) ")
 })
-app.listen(3000,()=>{
-console.log("server started")
+app.get("/Bonjour",function(req,res){
+   res.send("le serveur dit bonjour:) ")
 })
+
+app.use(express.urlencoded({extended:true}))
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('now listening for requests on port',process.env.PORT);})
